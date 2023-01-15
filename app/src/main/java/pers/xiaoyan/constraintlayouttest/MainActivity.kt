a8d15065790d401b9490df4038f280381cd509d5
ooxiaoyan <810978527@qq.com>
2023-01-15 17:21:11 +0800

    [Feature] Add layer

--- app/src/main/java/pers/xiaoyan/constraintlayouttest/MainActivity.kt
+++ app/src/main/java/pers/xiaoyan/constraintlayouttest/MainActivity.kt
@@ -2,10 +2,20 @@ package pers.xiaoyan.constraintlayouttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import pers.xiaoyan.constraintlayouttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.layerButton.setOnClickListener {
+            binding.layer.visibility = View.VISIBLE
+        }
     }
 }
